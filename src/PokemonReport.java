6f30d793940ac1e5bc5868d23f5733a8b67896ae
rhirwa <rhirwa2@huskers.unl.edu>
2022-11-28 12:03:31 -0600

    updated maximumSpeed method

--- src/PokemonReport.java
+++ src/PokemonReport.java
@@ -5,7 +5,11 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class PokemonReport {
-
+	/**
+	 * this method takes in an array list of speed recordings, and returns the maximum speed
+	 * @param list
+	 * @return
+	 */
 	public static String maximumSpeed(ArrayList<String> list) {
 		String pokemon = null;
 		int max = 0;
@@ -23,6 +27,25 @@ public class PokemonReport {
 		}
 		return pokemon;
 	}
+	
+	public static int maxSpeedNum(ArrayList<String> list) {
+		int max = 0;
+		for(int i=0; i<list.size(); i+=6) {
+			if(list.get(i+4).isBlank()) {
+				list.set(i, "0");
+			}
+			int numInt = Integer.parseInt(list.get(i+4));
+			if(numInt > max) {
+				max = numInt;
+				}
+		}
+		return max;
+	}
+	/**
+	 * This method takes in a list of Pokemon's attack recordings and returns their average
+	 * @param list
+	 * @return
+	 */
 	public static double averageAttack(ArrayList<String> list) {
 		double av = 0;
 		double sum = 0;
@@ -39,31 +62,49 @@ public class PokemonReport {
 		av = sum/count;
 		return av;
 	}
+	
+	/**
+	 * This method takes in a list of Pokemon's types and returns their sorted list along with how often they appear
+	 * @param list
+	 * @return
+	 */
 	public static ArrayList<String> sortedListWithCount(ArrayList<String> list) {
 		ArrayList<String> types = new ArrayList<>();
+		String bugCount = null;
 		String bug = null;
-		String fairy = null;
+		String fairyCount = null;
 		String normal = null;
+		String normalCount = null;
+		String fairy = null;
+		String psychicCount = null;
 		String psychic = null;
+		String steelCount = null;
 		String steel = null;
+		String waterCount = null;
 		String water = null;
 		for(int i = 1; i<list.size(); i+=6) {
 			if(list.get(i).equals("Bug")) {
-				bug = list.get(i).concat(": 36");
+				bugCount = String.valueOf(Collections.frequency(list, list.get(i)));
+				bug = list.get(i).concat("             " + bugCount);
 			} else if(list.get(i).equals("Fairy")){
-				fairy = list.get(i).concat(": 65");
+				fairyCount = String.valueOf(Collections.frequency(list, list.get(i)));
+				fairy = list.get(i).concat("             " + fairyCount);
 			} else if(list.get(i).equals("Normal")){
-				normal = list.get(i).concat(": 130");
+				normalCount = String.valueOf(Collections.frequency(list, list.get(i)));
+				normal = list.get(i).concat("             " + normalCount);
 			} else if(list.get(i).equals("Psychic")){
-				psychic = list.get(i).concat(": 123");
+				psychicCount = String.valueOf(Collections.frequency(list, list.get(i)));
+				psychic = list.get(i).concat("             " + psychicCount);
 			} else if(list.get(i).equals("Steel")){
-				steel = list.get(i).concat(": 74");
+				steelCount = String.valueOf(Collections.frequency(list, list.get(i)));
+				steel = list.get(i).concat("             " + steelCount);
 			} else {
-				water = list.get(i).concat(": 159");
+				waterCount = String.valueOf(Collections.frequency(list, list.get(i)));
+				water = list.get(i).concat("             " + waterCount);
 			}
 		}
-		types.add(normal);
 		types.add(bug);
+		types.add(normal);
 		types.add(fairy);
 		types.add(psychic);
 		types.add(steel);
@@ -95,8 +136,15 @@ public class PokemonReport {
 			System.err.println("File not found");
 			throw new RuntimeException (e);
 		}
-		System.out.println("The Pokemon with the maximum speed is: " + maximumSpeed(dataList));
-		System.out.printf("The average attack for 3rd generation Pokemons is: %.2f \n", averageAttack(dataList));
-		System.out.println("The sorted list of Pokemon types with count is: \n" + sortedListWithCount(dataList));
+		System.out.println("========================");
+		System.out.println(maximumSpeed(dataList) + " has speed of " + maxSpeedNum(dataList));
+		System.out.println("========================");
+		System.out.printf("Average attack for 3rd generation Pokemon's is %.2f \n", averageAttack(dataList));
+		System.out.println("========================");
+		System.out.println("                        ");
+		System.out.println("Type           Count");
+		for(int i= 0; i<sortedListWithCount(dataList).size(); i++) {
+			System.out.printf("%s \n", sortedListWithCount(dataList).get(i));
+		}
 	}
 }
